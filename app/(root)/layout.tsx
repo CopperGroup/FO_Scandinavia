b484e0b5a625d2b83ea4676310088dbf2cd0ccd5
tymyevhenmpw <tymyevhen@gmail.com>
2025-03-15 19:14:02 +0100

    Commit

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -40,7 +40,7 @@ export default async function RootLayout({
   const user = await fetchUserByEmail({email});
   
   return (
-      <html lang="ua">
+      <html lang="uk">
         <body className={inter.className}>
           {/* <Analytics /> */}
           <FacebookPixel />
