f1d183fb04a31b546b53c2b4b562bbda245a1194
tymyevhenmpw <tymyevhen@gmail.com>
2025-07-22 17:33:41 +0200

    COmmit

--- components/PostHogProvider.tsx
+++ components/PostHogProvider.tsx
@@ -6,12 +6,16 @@ import { useEffect } from "react"
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
+    console.log(process.env.NEXT_PUBLIC_POSTHOG_KEY)
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
       api_host: "/ingest",
       ui_host: "https://eu.posthog.com",
       defaults: '2025-05-24',
       capture_exceptions: true, // enables capturing exceptions using Error Tracking
       debug: process.env.NODE_ENV === "development",
+      loaded: (ph) => {
+        window.posthog = ph
+      },
     })
   }, [])
 
